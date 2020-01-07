cdca8b5dacefcaad67dfc4e15764e48c299bc702
adrianagraybill <adriana.graybill@gmail.com>
2020-01-06 18:49:47 -0800

    clipboard route

--- bucket-pasta-server/index.js
+++ bucket-pasta-server/index.js
@@ -1,33 +1,30 @@
 'use strict'
-const express = require('express')
+const express = require('express');
 const clipboard = require('./resources/sample-clipboard.js');
 
 // Create the express app
-const app = express()
+const app = express();
 
 // Routes and middleware
 // app.use(/* ... */)
-app.get('/getClipboard', function (req,res) {
-  response.send(clipboard);
+app.get('/clipboard', function (req,res) {
+  res.send(clipboard);
   res.status(200).send();
 });
 
-console.log(clipboard);
-
 // Error handlers
 app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
+  res.status(404).send();
 })
 app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
+  console.error(err);
+  res.status(500).send();
 })
 
 // Start server
 app.listen(1234, function (err) {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-
-  console.log('Started at http://localhost:1234')
+  console.log('Started at http://localhost:1234');
 })
