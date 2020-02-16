6357fe1cedc111d5b5d4e7c0b0c835e19343285f
Awood549 <Austin.wood1776@live.com>
2020-02-16 11:56:42 -0800

    created tabs model

--- bucket-pasta-server/src/models/pasties/pasties-schema.js
+++ bucket-pasta-server/src/models/pasties/pasties-schema.js
@@ -3,24 +3,11 @@
 const mongoose = require('mongoose');
 
 const pasties = mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-}, { toObject: { virtuals: true }, toJSON: { virtuals: true } });
+    displayName: String,
+    theme: String,
+    type: String,
+    content: String
+})
 
-pasties.virtual('tasks', {
-    ref: 'todo',
-    localField: 'name',
-    foreignField: 'category',
-    justOne: false,
-});
-
-pasties.pre('find', function () {
-    try {
-        this.populate('tasks');
-    }
-    catch (e) {
-        console.error('Find Error', e);
-    }
-});
 
 module.exports = mongoose.model('Pasties', pasties);
